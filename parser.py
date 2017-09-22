5a340d4a97d2f211e849be98656aa5ab15fa023a
piresjo <piresjo@seas.upenn.edu>
2017-09-22 13:36:39 -0400

    allow for different headers and directories via command args

--- parser.py
+++ parser.py
@@ -4,29 +4,35 @@ import time
 from bs4 import BeautifulSoup
 import re
 
-myArray = []
-resultList = ["start"]
+def main(argv):
+    header = argv[1]
+    targetDir = argv[2]
+    myArray = []
+    resultList = ["start"]
 
-header = "http://archiveofourown.org/tags/Star%20Wars%20Episode%20VII:%20The%20Force%20Awakens%20(2015)/works?page=" #can replace with url for any other fandom
-end = 1
+    #header = "http://archiveofourown.org/tags/Star%20Wars%20Episode%20VII:%20The%20Force%20Awakens%20(2015)/works?page=" #can replace with url for any other fandom
+    end = 1
 
-os.chdir(#insert target directory here)
+    #insert target directory here
+    os.chdir(targetDir)
 
-while (len(resultList)) != 0:
-    page1 = requests.get(header + str(end))
-    soup1 = BeautifulSoup(page1.text,"lxml")
+    while (len(resultList)) != 0:
+        page1 = requests.get(header + str(end))
+        soup1 = BeautifulSoup(page1.text,"lxml")
+        resultList = soup1(attrs = {'href' : re.compile('.*works/[0-9]+[0-9]$')})
 
-    resultList = soup1(attrs = {'href' : re.compile('.*works/[0-9]+[0-9]$')})
+        for x in resultList:
+            body = str(x).split('"')
+            docID = str(body[1]).split('/')
+            page2 = requests.get("http://archiveofourown.org/" + body[1] + "?view_adult=true&view_full_work=true")
+            filename = str(docID[2]) + '.html'
+            f = open(filename,'w', encoding='UTF')
+            f.write(str(page2.text))
+            f.close()
+            print("reached document " + str(docID[2]) + " on page " + str(end))
 
-    for x in resultList:
-        body = str(x).split('"')
-        docID = str(body[1]).split('/')
-        page2 = requests.get("http://archiveofourown.org/" + body[1] + "?view_adult=true&view_full_work=true")
-        filename = str(docID[2]) + '.html'
-        f = open(filename,'w', encoding='UTF')
-        f.write(str(page2.text))
-        f.close()
-        print("reached document " + str(docID[2]) + " on page " + str(end))
+        time.sleep(1)
+        end += 1
 
-    time.sleep(1)
-    end += 1
+if __name__ == "__main__":
+    main(sys.argv)
