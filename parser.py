013cd81222cce048359effbe8ac8e3473122375a
piresjo <piresjo@seas.upenn.edu>
2017-09-29 13:13:31 -0400

    switch to argparse

--- parser.py
+++ parser.py
@@ -5,38 +5,45 @@ from bs4 import BeautifulSoup
 import re
 import sys
 import io
+import argparse
 
-def main(argv):
-    header = argv[1]
-    targetDir = argv[2]
-    myArray = []
-    resultList = ["start"]
+parser = argparse.ArgumentParser(description='Parse Fanfiction')
+parser.add_argument('header', type=str, nargs=1, help="header to actually scrape the files")
+#parser.add_argument('-s', action='store_true', help='determine if we need to search for an actual header')
+parser.add_argument('targetdir', type=str, nargs=1, help="place to put the scraped files")
+args = parser.parse_args()
+argv = vars(args)
 
-    #header = "http://archiveofourown.org/tags/Star%20Wars%20Episode%20VII:%20The%20Force%20Awakens%20(2015)/works?page=" #can replace with url for any other fandom
-    end = 1
+header = argv['header'][0]
+targetDir = argv['targetdir'][0]
+myArray = []
+resultList = ["start"]
 
-    #insert target directory here
-    os.chdir(targetDir)
+# header =
+# "http://archiveofourown.org/tags/Star%20Wars%20Episode%20VII:%20The%20Force%20Awakens%20(2015)/works?page="
+# #can replace with url for any other fandom
+end = 1
 
-    while (len(resultList)) != 0:
-        page1 = requests.get(header + str(end))
-        soup1 = BeautifulSoup(page1.text,"lxml")
-        resultList = soup1(attrs = {'href' : re.compile('.*works/[0-9]+[0-9]$')})
+# insert target directory here
+os.chdir(targetDir)
 
-        for x in resultList:
-            body = str(x).split('"')
-            docID = str(body[1]).split('/')
-            page2 = requests.get("http://archiveofourown.org/" + body[1] + "?view_adult=true&view_full_work=true")
-            filename = str(docID[2]) + '.html'
-            #Is encoding only in Python 3?
-            f = open(filename,'w', encoding='UTF')
+while (len(resultList)) != 0:
+	page1 = requests.get(header + str(end))
+	soup1 = BeautifulSoup(page1.text, "lxml")
+	resultList = soup1(attrs={'href': re.compile('.*works/[0-9]+[0-9]$')})
 
-            f.write(str(page2.text))
-            f.close()
-            print("reached document " + str(docID[2]) + " on page " + str(end))
+	for x in resultList:
+		body = str(x).split('"')
+		docID = str(body[1]).split('/')
+		page2 = requests.get("http://archiveofourown.org/" +
+								 body[1] + "?view_adult=true&view_full_work=true")
+		filename = str(docID[2]) + '.html'
+		# Is encoding only in Python 3?
+		f = open(filename, 'w', encoding='UTF')
 
-        time.sleep(1)
-        end += 1
+		f.write(str(page2.text))
+		f.close()
+		print("reached document " + str(docID[2]) + " on page " + str(end))
 
-if __name__ == "__main__":
-    main(sys.argv)
+		time.sleep(1)
+		end += 1
